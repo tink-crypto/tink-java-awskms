8f9f19e12b08cb5f00b06f5a28f34db9d55506db
Moreno Ambrosin <ambrosin@google.com>
2022-10-05 12:13:24 -0700

    Add POM file and Maven deploy script to tink-java-awskms, and Kokoro test script.
    PiperOrigin-RevId: 479110059
    Change-Id: I99662478c3e791726fa95eb3d50b7425263ef106

---
+++ kokoro/gcp_ubuntu/maven/run_tests.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+set -euo pipefail
+
+if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]] ; then
+  TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
+  cd "${TINK_BASE_DIR}/tink_java_awskms"
+  use_bazel.sh "$(cat .bazelversion)"
+fi
+
+: "${TINK_BASE_DIR:=$(cd .. && pwd)}"
+
+# Check for dependencies in TINK_BASE_DIR. Any that aren't present will be
+# downloaded.
+readonly GITHUB_ORG="https://github.com/tink-crypto"
+./kokoro/testutils/fetch_git_repo_if_not_present.sh "${TINK_BASE_DIR}" \
+  "${GITHUB_ORG}/tink-java"
+
+./kokoro/testutils/update_android_sdk.sh
+./kokoro/testutils/replace_http_archive_with_local_repository.py \
+  -f "WORKSPACE" \
+  -t "${TINK_BASE_DIR}"
+# Install the latest snapshot locally.
+./maven/maven_deploy_library.sh install tink-awskms \
+  maven/tink-java-awskms.pom.xml HEAD
+# Run tink-java's examples/helloworld against the local artifact.
+./kokoro/testutils/test_maven_snapshot.sh -l \
+  "${TINK_BASE_DIR}/tink_java/examples/helloworld/pom.xml"
