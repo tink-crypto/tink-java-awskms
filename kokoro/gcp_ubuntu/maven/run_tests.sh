40c149aa9804c4e8f9e3292effa7737713dbbe60
Moreno Ambrosin <ambrosin@google.com>
2022-10-05 13:34:37 -0700

    Install Python3 on maven test scripts.
    PiperOrigin-RevId: 479129965
    Change-Id: Ic57e7462b080c5642f1da503ff5525fdf2877a9e

--- kokoro/gcp_ubuntu/maven/run_tests.sh
+++ kokoro/gcp_ubuntu/maven/run_tests.sh
@@ -30,6 +30,7 @@ readonly GITHUB_ORG="https://github.com/tink-crypto"
 ./kokoro/testutils/fetch_git_repo_if_not_present.sh "${TINK_BASE_DIR}" \
   "${GITHUB_ORG}/tink-java"
 
+source ./kokoro/testutils/install_python3.sh
 ./kokoro/testutils/update_android_sdk.sh
 ./kokoro/testutils/replace_http_archive_with_local_repository.py \
   -f "WORKSPACE" \
