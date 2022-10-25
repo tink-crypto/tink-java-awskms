b0a02ce8fc909098fc6e67a111f5fa02d4228ea7
Moreno Ambrosin <ambrosin@google.com>
2022-10-25 04:40:58 -0700

    Use injected `use_bazel.sh` script when running on Kokoro
    PiperOrigin-RevId: 483637527
    Change-Id: I449a1421c8797518134fab17ffd2b6129c0aee44

--- kokoro/gcp_ubuntu/bazel/run_tests.sh
+++ kokoro/gcp_ubuntu/bazel/run_tests.sh
@@ -86,7 +86,8 @@ main() {
   if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]] ; then
     TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
     cd "${TINK_BASE_DIR}/tink_java_awskms"
-    use_bazel.sh "$(cat .bazelversion)"
+    chmod +x "${KOKORO_GFILE_DIR}/use_bazel.sh"
+    "${KOKORO_GFILE_DIR}/use_bazel.sh" "$(cat .bazelversion)"
   fi
 
   : "${TINK_BASE_DIR:=$(cd .. && pwd)}"
