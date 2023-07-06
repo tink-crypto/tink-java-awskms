320b38c2faea6b45146e3971bc33bb9ec965008e
Moreno Ambrosin <ambrosin@google.com>
2023-07-06 05:02:35 -0700

    Remove non-Java container images from tink_test_container_images.sh
    Also rename tink_test_container_images.sh to java_test_container_images.sh
    
    PiperOrigin-RevId: 545952063
    Change-Id: Iaf1f3c59446aeeb909e0bb9603b484ff452d737c

--- kokoro/gcp_ubuntu/bazel/run_tests.sh
+++ kokoro/gcp_ubuntu/bazel/run_tests.sh
@@ -33,7 +33,7 @@ RUN_COMMAND_ARGS=()
 if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]] ; then
   TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
   source \
-    "${TINK_BASE_DIR}/tink_java_awskms/kokoro/testutils/tink_test_container_images.sh"
+    "${TINK_BASE_DIR}/tink_java_awskms/kokoro/testutils/java_test_container_images.sh"
   CONTAINER_IMAGE="${TINK_JAVA_BASE_IMAGE}"
   RUN_COMMAND_ARGS+=( -k "${TINK_GCR_SERVICE_KEY}" )
 fi
