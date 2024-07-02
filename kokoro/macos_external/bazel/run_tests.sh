2b3fa9a252a796cf24af12ddea518c1d08aff736
Moreno Ambrosin <ambrosin@google.com>
2024-07-02 05:26:26 -0700

    Install a more recent version of the Android cmdline-tools in macOS tests
    PiperOrigin-RevId: 648683213
    Change-Id: I8e4c5bda02afb34a2d0aa3bd6afb0bf581e15d11

--- kokoro/macos_external/bazel/run_tests.sh
+++ kokoro/macos_external/bazel/run_tests.sh
@@ -16,15 +16,12 @@
 
 set -euo pipefail
 
-export XCODE_VERSION="14.1"
-export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
-export ANDROID_HOME="/usr/local/share/android-sdk"
-export COURSIER_OPTS="-Djava.net.preferIPv6Addresses=true"
-
 if [[ -n "${KOKORO_ROOT:-}" ]] ; then
   readonly TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
   cd "${TINK_BASE_DIR}/tink_java_awskms"
   export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-8-latest/Contents/Home
+  export XCODE_VERSION="14.1"
+  export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
 fi
 
 BAZEL_TEST_ARGS=()
@@ -39,5 +36,5 @@ BAZEL_TEST_ARGS+=( . )
 
 readonly BAZEL_TEST_ARGS
 
-./kokoro/testutils/update_android_sdk.sh
+source ./kokoro/testutils/update_android_sdk.sh
 ./kokoro/testutils/run_bazel_tests.sh "${BAZEL_TEST_ARGS[@]}"
