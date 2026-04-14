c69365e75ad8b3d6e94eba236aaa9df589b92da8
Juerg Wullschleger <juerg@google.com>
2026-04-14 07:55:19 -0700

    Add ids to scripts.
    PiperOrigin-RevId: 899587267
    Change-Id: Ia93a5cc66c085714484c94ed5a88167efd825255

--- kokoro/release_maven.sh
+++ kokoro/release_maven.sh
@@ -14,7 +14,12 @@
 # limitations under the License.
 ################################################################################
 
-set -euo pipefail
+# Generated with openssl rand -hex 10
+echo "================================================================================"
+echo "Tink Script ID: fb3b150c1c0154215c1a (to quickly find the script from logs)"
+echo "================================================================================"
+
+set -eEuo pipefail
 
 # Fail if RELEASE_VERSION is not set.
 if [[ -z "${RELEASE_VERSION:-}" ]]; then
